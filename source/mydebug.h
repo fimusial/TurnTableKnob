0d79d15cbcf603599ee68f691c68451589cd055e
Filip Musiał <fimusial@gmail.com>
2026-01-25 21:10:42 +0100

    mydebug v2

--- source/mydebug.h
+++ source/mydebug.h
@@ -1,7 +1,9 @@
 #include "stdio.h"
 #include "windows.h"
 
-void print(double value, char ending = '\n')
+std::vector<double> _mydebug_prints(65536);
+
+void _mydebug_print(double value, char ending = '\n')
 {
     char format[4] = { '%', 'f', '\0', '\0' };
     format[2] = ending;
@@ -9,4 +11,17 @@ void print(double value, char ending = '\n')
     memset(buffer, '\0', 26);
     sprintf(buffer, format, value);
     OutputDebugStringA(buffer);
+}
+
+void _mydebug_push_print(double value)
+{
+    _mydebug_prints.push_back(value);
+}
+
+void _mydebug_flush_prints()
+{
+    for (size_t i = 0; i < _mydebug_prints.size(); i++)
+    {
+        _mydebug_print(_mydebug_prints[i]);
+    }
 }
